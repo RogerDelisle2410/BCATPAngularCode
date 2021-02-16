fb1219be0c9e22046bc6b07b9c7f876d236ac02d
Roger Delisle <jrd.consulting@hotmail.com>
2021-02-16 15:02:46 -0700

    Latest fast

--- BCATPAngular8EF/Startup.cs
+++ BCATPAngular8EF/Startup.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 //using Microsoft.AspNetCore.SpaServices.AngularCli;
 //using Microsoft.AspNetCore.SpaServices.Extensions;
-using Microsoft.AspNetCore.NodeServices;
+//using Microsoft.AspNetCore.NodeServices;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
