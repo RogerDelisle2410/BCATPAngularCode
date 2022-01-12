1d91518047a59b64c1e63c621e1614cc1d34e71a
Roger Delisle <jrd.consulting@hotmail.com>
2022-01-11 20:50:03 -0700

    Latest

--- BCATPAngular8EF/Models/TblBcatp.cs
+++ BCATPAngular8EF/Models/TblBcatp.cs
@@ -1,6 +1,14 @@
 ﻿namespace BCATPAngular8EF.Models
 {
-    public class TblBcatp
+    public class TblBcatp : TableFormat { }
+    public class TblNavy : TableFormat { }
+    public class TblDewline : TableFormat { }
+    public class TblMidCanada : TableFormat { }
+    public class TblPinetree : TableFormat { }
+    public class TblAirforce : TableFormat { }
+    public class TblArmy : TableFormat { }
+    public class TblDefunct : TableFormat { }
+    public class TableFormat
     {
         public int Id { get; set; }
         public string Name { get; set; }
@@ -9,68 +17,4 @@
         public string Comment { get; set; }
         public string Wiki { get; set; }
     }
-    public class TblNavy
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public double Longitude { get; set; }
-        public double Latitude { get; set; }
-        public string Comment { get; set; }
-        public string Wiki { get; set; }
-    }
-    public class TblDewline
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public double Longitude { get; set; }
-        public double Latitude { get; set; }
-        public string Comment { get; set; }
-        public string Wiki { get; set; }
-    }
-    public class TblMidCanada
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public double Longitude { get; set; }
-        public double Latitude { get; set; }
-        public string Comment { get; set; }
-        public string Wiki { get; set; }
-    }
-
-    public class TblPinetree
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public double Longitude { get; set; }
-        public double Latitude { get; set; }
-        public string Comment { get; set; }
-        public string Wiki { get; set; }
-    }
-    public class TblAirforce
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public double Longitude { get; set; }
-        public double Latitude { get; set; }
-        public string Comment { get; set; }
-        public string Wiki { get; set; }
-    }
-    public class TblArmy
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public double Longitude { get; set; }
-        public double Latitude { get; set; }
-        public string Comment { get; set; }
-        public string Wiki { get; set; }
-    }
-    public class TblDefunct
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public double Longitude { get; set; }
-        public double Latitude { get; set; }
-        public string Comment { get; set; }
-        public string Wiki { get; set; }
-    } 
 }
